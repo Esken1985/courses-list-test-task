8a9aac7b206eddce46a31458f6dfde472e44bb52
Eskender <esken999@gmail.com>
2020-11-23 07:20:44 +0300

    added core-js dependency

--- src/app/components/courses-list/courses-list.service.ts
+++ src/app/components/courses-list/courses-list.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http'
 import { ICourses } from '../../../classes/courses';
 import { Observable, throwError } from 'rxjs';
-import {catchError, map} from "rxjs/operators"
+import {catchError} from "rxjs/operators"
 
 
 @Injectable({
