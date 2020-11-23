23a1d406ec9e35257721451fdae0587b758de91d
Eskender <esken999@gmail.com>
2020-11-23 07:29:11 +0300

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js'
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
